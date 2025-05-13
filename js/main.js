aa6356f0d2c713fa2dc717ffd448560e4e6ef3d3
alisq <ali.shamas.qadeer@gmail.com>
2025-05-13 11:16:20 -0400

    Demo

--- js/main.js
+++ js/main.js
@@ -7,13 +7,13 @@ function fitter() {
       $text.css("font-size", "14px");
   
       const tW = $text.width();
-      const cW = $container.width()-100;
+      const cW = $container.width()-40;
       const ratio = cW / tW;
   
       let newSize = 14 * ratio;
   
-      if (newSize > 300) {
-        newSize = 300;
+      if (newSize > 250) {
+        newSize = 250;
       }
   
       $text.css("font-size", newSize + "px");
