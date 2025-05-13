7689ac1b42c0e586dce2122985c9312fc805e00e
alisq <ali.shamas.qadeer@gmail.com>
2025-05-13 10:55:12 -0400

    got basics working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-setTimeout(function(){$('.text').boxfit();},500);
+function fitter() {
+    $(".container").each(function () {
+      const $container = $(this);
+      const $text = $container.find(".text");
+  
+      // Temporarily reset font size to base
+      $text.css("font-size", "14px");
+  
+      const tW = $text.width();
+      const cW = $container.width()-100;
+      const ratio = cW / tW;
+  
+      let newSize = 14 * ratio;
+  
+      if (newSize > 300) {
+        newSize = 300;
+      }
+  
+      $text.css("font-size", newSize + "px");
+    });
+  }
+  
+  $(document).ready(function () {
+    fitter();
+    $(window).resize(fitter);
+    $(document).keypress(fitter);
+  });
+  
